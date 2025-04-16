b052a5b3be487273ed1e4a7d8a5a947dde056206
Samir Elboukaoui <selbouka@e2r13p1.1337.ma>
2025-04-16 22:53:04 +0100

    tomorrooooow will be done

--- parsing/handle_signals.c
+++ parsing/handle_signals.c
@@ -6,7 +6,7 @@
 /*   By: selbouka <selbouka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 18:34:48 by selbouka          #+#    #+#             */
-/*   Updated: 2025/04/16 19:26:01 by selbouka         ###   ########.fr       */
+/*   Updated: 2025/04/16 22:51:51 by selbouka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,30 @@
 // Create main function of signals
 // Create handling functions of ^\ and ^C
 
-
-
-
-
-
-
-void    setup_signals(void)
+void    parent_handler(int signum)
 {
     
 }
+
+void    setup_signals(int mode)
+{
+    struct sigaction sg;
+
+    if (mode == 1) // parent
+    {
+        sg.sa_handler = parent_handler;
+        sg.sa_flags = SA_RESTART;
+        sigaction(SIGINT, &sg, NULL);
+        sg.sa_handler = SIG_IGN;
+        sigaction(SIGQUIT, &sg, NULL);
+    }
+    else
+    {
+        sg.sa_handler = SIG_DFL;
+        sg.sa_flags = 0;
+        sigaction(SIGINT, &sg, NULL);
+        sg.sa_handler = SIG_IGN;
+        sigaction(SIGQUIT, &sg, NULL);
+    }
+}
+
