a8c3506dad0950d74668bceed851b463028acf05
Samir Elboukaoui <selbouka@e2r4p2.1337.ma>
2025-04-18 13:11:21 +0100

    add strcmp

--- utils/ft_strcmp.c
+++ utils/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: selbouka <selbouka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:06:26 by selbouka          #+#    #+#             */
-/*   Updated: 2025/04/17 17:07:23 by selbouka         ###   ########.fr       */
+/*   Updated: 2025/04/18 13:08:08 by selbouka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,15 @@
 
 int ft_strcmp(char *s1, char *s2)
 {
-    return 1;
+    size_t	i;
+
+	i = 0;
+	while (((unsigned char)s1[i] || (unsigned char)s2[i]))
+	{
+		if ((unsigned char)s1[i] == (unsigned char) s2[i])
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
