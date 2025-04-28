4e71226513241440ec0550a175a98b78443c5d3f
Abderrahmane Sebban <asebban@e1r10p11.1337.ma>
2025-04-28 11:32:44 +0100

    echo_handler

--- builtins_fun/echo_builtin.c
+++ builtins_fun/echo_builtin.c
@@ -6,34 +6,37 @@
 /*   By: asebban <asebban@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:50:49 by asebban           #+#    #+#             */
-/*   Updated: 2025/04/21 13:51:14 by asebban          ###   ########.fr       */
+/*   Updated: 2025/04/28 11:30:46 by asebban          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	echo(char **args)
+int is_n_option(const char *str)
 {
-	int	i;
-	int	newline;
-	int	arg_count;
+	int i;
 
-	i = 1;
-	newline = 1;
-	arg_count = 0;
-	
-	// Count arguments and check for -n flag
-	while (args[arg_count])
-		arg_count++;
-	
-	// Handle -n option (must be first argument and exactly "-n")
-	if (args[1] && ft_strcmp(args[1], "-n") == 0)
+	if (!str || str[0] != '-' || str[1] != 'n')
+		return (0);
+	i = 2;
+	while (str[i] == 'n')
+		i++;
+	return (str[i] == '\0'); // valid if only '-' followed by one or more 'n'
+}
+
+int echo(char **args)
+{
+	int i = 1;
+	int newline = 1;
+
+	// Handle multiple -n options
+	while (args[i] && is_n_option(args[i]))
 	{
 		newline = 0;
 		i++;
 	}
-	
-	// Print all arguments separated by spaces
+
+	// Print arguments
 	while (args[i])
 	{
 		ft_putstr_fd(args[i], STDOUT_FILENO);
@@ -41,10 +44,9 @@ int	echo(char **args)
 			ft_putchar_fd(' ', STDOUT_FILENO);
 		i++;
 	}
-	
-	// Print newline unless -n was specified
+
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
-	
+
 	return (EXIT_SUCCESS);
 }
