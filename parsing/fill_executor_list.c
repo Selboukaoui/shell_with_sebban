58bf782b2ee8f14804d65eb229abc5229e85d8bb
Abderrahmane Sebban <asebban@e1r13p1.1337.ma>
2025-05-01 13:46:56 +0100

    sdfgk

--- parsing/fill_executor_list.c
+++ parsing/fill_executor_list.c
@@ -6,7 +6,7 @@
 /*   By: asebban <asebban@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:18:37 by asebban           #+#    #+#             */
-/*   Updated: 2025/05/01 10:54:56 by asebban          ###   ########.fr       */
+/*   Updated: 2025/05/01 13:42:37 by asebban          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,159 +135,420 @@ int ft_isspace(int c)
 //     return 0; // Not found or not quoted
 // }
 
-static int is_delimiter_quoted_in_cmd(const char *cmdline, const char *delimiter, int heredoc_index)
+// static int is_delimiter_quoted_in_cmd(const char *cmdline, const char *delimiter, int heredoc_index)
+// {
+//     int i = 0;
+//     int count = 0;
+//     int in_single = 0, in_double = 0;
+
+//     (void)delimiter;
+//     while (cmdline[i])
+//     {
+//         if (cmdline[i] == '\'' && !in_double)
+//         {
+//             in_single = !in_single;
+//             i++;
+//             continue;
+//         }
+//         if (cmdline[i] == '"' && !in_single)
+//         {
+//             in_double = !in_double;
+//             i++;
+//             continue;
+//         }
+
+//         if (!in_single && !in_double && cmdline[i] == '<' && cmdline[i + 1] == '<')
+//         {
+//             i += 2;
+//             while (cmdline[i] && ft_isspace(cmdline[i]))
+//                 i++;
+
+//             // Extract delimiter token from the command line
+//             int start = i;
+//             while (cmdline[i] && !ft_isspace(cmdline[i]) && cmdline[i] != '|' && cmdline[i] != '<' && cmdline[i] != '>')
+//                 i++;
+//             int end = i;
+
+//             if (count == heredoc_index)
+//             {
+//                 char *raw_delim = ft_substr(cmdline, start, end - start);
+//                 int quoted = 0;
+
+//                 if (raw_delim)
+//                 {
+//                     size_t len = ft_strlen(raw_delim);
+//                     // Consider quoted if delimiter contains any quotes or is surrounded by quotes
+//                     if ((len >= 2 && ((raw_delim[0] == '\'' && raw_delim[len - 1] == '\'') ||
+//                                      (raw_delim[0] == '"' && raw_delim[len - 1] == '"'))) ||
+//                         ft_strchr(raw_delim, '\'') || ft_strchr(raw_delim, '"'))
+//                     {
+//                         quoted = 1;
+//                     }
+//                     free(raw_delim);
+//                 }
+//                 return quoted;
+//             }
+//             count++;
+//         }
+//         else
+//         {
+//             i++;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+// static char *strip_quotes(const char *str)
+// {
+//     size_t len = ft_strlen(str);
+
+//     if (len >= 2 && ((str[0] == '\'' && str[len - 1] == '\'') ||
+//                      (str[0] == '"' && str[len - 1] == '"')))
+//     {
+//         return ft_substr(str, 1, len - 2); // allocates a new string without quotes
+//     }
+//     return ft_strdup(str); // no quotes, just duplicate it
+// }
+
+// char *get_last_heredoc_delimiter(const char *cmdline) {
+//     char *last = NULL;
+
+//     for (size_t i = 0; cmdline[i] != '\0'; ++i) {
+//         if (cmdline[i] == '<' && cmdline[i+1] == '<') {
+//             i += 2;
+//             /* skip any spaces */
+//             while (cmdline[i] && isspace((unsigned char)cmdline[i]))
+//                 ++i;
+
+//             /* start of delimiter */
+//             size_t start = i;
+//             while (cmdline[i] &&
+//                    !isspace((unsigned char)cmdline[i]) &&
+//                    cmdline[i] != '<' &&
+//                    cmdline[i] != '>' &&
+//                    cmdline[i] != '|') {
+//                 ++i;
+//             }
+//             size_t len = i - start;
+//             if (len > 0) {
+//                 /* free previous, keep only the last */
+//                 free(last);
+//                 last = strndup(cmdline + start, len);
+//             }
+//         }
+//     }
+//     return last;
+// }
+// static int create_heredoc(char *delimiter, t_shell *shell, int heredoc_index)
+// {
+//     int     fd[2];
+//     char    *line;
+//     int     quoted;
+//     char    *real_delim;
+
+//     if (pipe(fd) == -1)
+//         return (-1);
+
+//     // Check if the delimiter was quoted in the command line
+//     quoted = is_delimiter_quoted_in_cmd(shell->rl_copy, delimiter, heredoc_index);
+//     real_delim = strip_quotes(delimiter);
+
+//     while (1)
+//     {
+//         line = readline("> ");
+//         if (!line || ft_strcmp(line, real_delim) == 0)
+//         {
+//             if (!line)
+//             {
+//                 ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `",2);
+//                 ft_putstr_fd(delimiter,2);
+//                 ft_putstr_fd("\')\n", 2);
+//             }
+//             free(line);
+//             break;
+//         }
+
+//         if (!quoted)
+//         {
+//             char *expanded = replace_vars(line, shell);
+//             write(fd[1], expanded, ft_strlen(expanded));
+//             write(fd[1], "\n", 1);
+//             free(expanded);
+//         }
+//         else
+//         {
+//             write(fd[1], line, ft_strlen(line));
+//             write(fd[1], "\n", 1);
+//         }
+//         free(line);
+//     }
+
+//     free(real_delim);
+//     close(fd[1]);
+//     return (fd[0]);
+// }
+
+
+// static char *get_last_heredoc_delimiter(const char *cmdline)
+// {
+//     char *last = NULL;
+//     for (size_t i = 0; cmdline[i]; ++i) {
+//         if (cmdline[i] == '<' && cmdline[i + 1] == '<') {
+//             i += 2;
+//             while (cmdline[i] && ft_isspace(cmdline[i]))
+//                 ++i;
+//             size_t start = i;
+//             while (cmdline[i] && !ft_isspace(cmdline[i])
+//                    && cmdline[i] != '<' && cmdline[i] != '>' && cmdline[i] != '|')
+//                 ++i;
+//             size_t len = i - start;
+//             if (len > 0) {
+//                 free(last);
+//                 last = strndup(cmdline + start, len);
+//             }
+//         }
+//     }
+//     return last;
+// }
+
+// /**
+//  * Return 1 if the *last* heredoc delimiter in cmdline is wrapped in matching
+//  * single or double quotes; 0 otherwise.
+//  */
+// static int is_last_delim_quoted(const char *cmdline)
+// {
+//     char *raw = get_last_heredoc_delimiter(cmdline);
+//     if (!raw) return 0;
+//     size_t n = ft_strlen(raw);
+//     int quoted = (n >= 2 &&
+//                  ((raw[0] == '\'' && raw[n-1] == '\'') ||
+//                   (raw[0] == '"' && raw[n-1] == '"')));
+//     free(raw);
+//     return quoted;
+// }
+
+// /**
+//  * Strip matching outer single or double quotes from s.
+//  * Returns newly allocated unquoted string, or strdup(s) if no quotes.
+//  */
+// static char *strip_quotes(const char *s)
+// {
+//     size_t n = ft_strlen(s);
+//     if (n >= 2 &&
+//        ((s[0] == '\'' && s[n-1] == '\'') ||
+//         (s[0] == '"' && s[n-1] == '"'))) {
+//         return ft_substr(s, 1, n - 2);
+//     }
+//     return ft_strdup(s);
+// }
+
+// /**
+//  * Create a heredoc: read lines until the given delimiter (quotes stripped).
+//  * Variables are expanded only if this delimiter is NOT the last one that was quoted.
+//  * Returns read-end FD of the pipe, or -1 on error.
+//  */
+// int create_heredoc(const char *delimiter, t_shell *shell)
+// {
+//     int pipefd[2];
+//     if (pipe(pipefd) == -1)
+//         return -1;
+
+//     int quoted = is_last_delim_quoted(shell->rl_copy);
+//     char *real_delim = strip_quotes(delimiter);
+
+//     while (1) {
+//         char *line = readline("> ");
+//         if (!line || ft_strcmp(line, real_delim) == 0) {
+//             free(line);
+//             break;
+//         }
+//         if (!quoted) {
+//             char *expanded = replace_vars(line, shell);
+//             write(pipefd[1], expanded, ft_strlen(expanded));
+//             write(pipefd[1], "\n", 1);
+//             free(expanded);
+//         } else {
+//             write(pipefd[1], line, ft_strlen(line));
+//             write(pipefd[1], "\n", 1);
+//         }
+//         free(line);
+//     }
+
+//     free(real_delim);
+//     close(pipefd[1]);
+//     return pipefd[0];
+// }
+
+// /**
+//  * Process input redirection: if HEREDOC, invoke create_heredoc with the
+//  * raw token after HEREDOC; otherwise open filename. Returns OK or FAILED.
+//  */
+// static int process_in_heredoc(t_executor *cur, t_lexer_list *lex, t_shell *sh)
+// {
+//     if (lex->type == HEREDOC) {
+//         cur->fd_in = create_heredoc(lex->next->str, sh);
+//         if (cur->fd_in == -1)
+//             return FAILED;
+//     } else {
+//         cur->fd_in = open(lex->next->str, O_RDONLY);
+//         if (cur->fd_in == -1) {
+//             ft_putstr_fd("minishell: ", STDERR_FILENO);
+//             perror(lex->next->str);
+//             return FAILED;
+//         }
+//     }
+//     return OK;
+// }
+
+
+// static int process_in_heredoc(t_executor *current, t_lexer_list *lexer, t_shell *shell)
+// {
+//     // if (!lexer->next || lexer->next->type != EMPTY)
+//     // {
+//     //     ft_putstr_fd("minishell: syntax error near unexpected token\n", STDERR_FILENO);
+//     //     return (FAILED);
+//     // }
+//     int heredoc_index = 0;
+//     if (lexer->type == HEREDOC)
+//     {
+//         current->fd_in = create_heredoc(lexer->next->str, shell, heredoc_index++);
+//         if (current->fd_in == -1)
+//             return (FAILED);
+//     }
+//     else
+//     {
+//         current->fd_in = open(lexer->next->str, O_RDONLY);
+//         if (current->fd_in == -1)
+//         {
+//             ft_putstr_fd("minishell: ", STDERR_FILENO);
+//             perror(lexer->next->str);
+//             return (FAILED);
+//         }
+//     }
+//     return (OK);
+// }
+
+static char *get_last_heredoc_delimiter(const char *cmdline)
 {
-    int i = 0;
-    int count = 0;
-    int in_single = 0, in_double = 0;
-
-    (void)delimiter;
-    while (cmdline[i])
-    {
-        if (cmdline[i] == '\'' && !in_double)
-        {
-            in_single = !in_single;
-            i++;
-            continue;
-        }
-        if (cmdline[i] == '"' && !in_single)
-        {
-            in_double = !in_double;
-            i++;
-            continue;
-        }
-
-        if (!in_single && !in_double && cmdline[i] == '<' && cmdline[i + 1] == '<')
-        {
+    char *last = NULL;
+    for (size_t i = 0; cmdline[i]; ++i) {
+        if (cmdline[i] == '<' && cmdline[i + 1] == '<') {
             i += 2;
             while (cmdline[i] && ft_isspace(cmdline[i]))
-                i++;
-
-            // Extract delimiter token from the command line
-            int start = i;
-            while (cmdline[i] && !ft_isspace(cmdline[i]) && cmdline[i] != '|' && cmdline[i] != '<' && cmdline[i] != '>')
-                i++;
-            int end = i;
-
-            if (count == heredoc_index)
-            {
-                char *raw_delim = ft_substr(cmdline, start, end - start);
-                int quoted = 0;
-
-                if (raw_delim)
-                {
-                    size_t len = ft_strlen(raw_delim);
-                    // Consider quoted if delimiter contains any quotes or is surrounded by quotes
-                    if ((len >= 2 && ((raw_delim[0] == '\'' && raw_delim[len - 1] == '\'') ||
-                                     (raw_delim[0] == '"' && raw_delim[len - 1] == '"'))) ||
-                        ft_strchr(raw_delim, '\'') || ft_strchr(raw_delim, '"'))
-                    {
-                        quoted = 1;
-                    }
-                    free(raw_delim);
-                }
-                return quoted;
+                ++i;
+            size_t start = i;
+            while (cmdline[i] && !ft_isspace(cmdline[i])
+                   && cmdline[i] != '<' && cmdline[i] != '>' && cmdline[i] != '|')
+                ++i;
+            size_t len = i - start;
+            if (len > 0) {
+                free(last);
+                last = strndup(cmdline + start, len);
             }
-            count++;
-        }
-        else
-        {
-            i++;
         }
     }
-
-    return 0;
+    return last;
 }
 
-
-static char *strip_quotes(const char *str)
+/**
+ * Return 1 if the *last* heredoc delimiter in cmdline contains any quote
+ * characters (single or double), indicating it was quoted; 0 otherwise.
+ */
+static int is_last_delim_quoted(const char *cmdline)
 {
-    size_t len = ft_strlen(str);
+    char *raw = get_last_heredoc_delimiter(cmdline);
+    if (!raw)
+        return 0;
+    int quoted = (strchr(raw, '\'') != NULL || strchr(raw, '"') != NULL);
+    free(raw);
+    return quoted;
+}
 
-    if (len >= 2 && ((str[0] == '\'' && str[len - 1] == '\'') ||
-                     (str[0] == '"' && str[len - 1] == '"')))
-    {
-        return ft_substr(str, 1, len - 2); // allocates a new string without quotes
+/**
+ * Strip all single and double quotes from the delimiter token.
+ * Returns newly allocated unquoted string, or strdup(s) if no quotes.
+ */
+static char *strip_quotes(const char *s)
+{
+    size_t len = ft_strlen(s);
+    char *out = malloc(len + 1);
+    if (!out) return NULL;
+    size_t j = 0;
+    for (size_t i = 0; i < len; ++i) {
+        if (s[i] != '\'' && s[i] != '"')
+            out[j++] = s[i];
     }
-    return ft_strdup(str); // no quotes, just duplicate it
+    out[j] = '\0';
+    return out;
 }
 
-
-static int create_heredoc(char *delimiter, t_shell *shell, int heredoc_index)
+/**
+ * Create a heredoc: read lines until the given delimiter (quotes stripped).
+ * Variables are expanded only if the *last* delimiter was not quoted.
+ * Prints warning on EOF (Ctrl-D) and returns read-end FD, or -1 on error.
+ */
+int create_heredoc(char *delimiter, t_shell *shell)
 {
-    int     fd[2];
-    char    *line;
-    int     quoted;
-    char    *real_delim;
+    int pipefd[2];
+    if (pipe(pipefd) == -1)
+        return -1;
 
-    if (pipe(fd) == -1)
-        return (-1);
+    int quoted = is_last_delim_quoted(shell->rl_copy);
+    char *real_delim = strip_quotes(delimiter);
+    if (!real_delim)
+        return -1;
 
-    // Check if the delimiter was quoted in the command line
-    quoted = is_delimiter_quoted_in_cmd(shell->rl_copy, delimiter, heredoc_index);
-    real_delim = strip_quotes(delimiter);
-
-    while (1)
-    {
-        line = readline("> ");
-        if (!line || ft_strcmp(line, real_delim) == 0)
-        {
-            if (!line)
-            {
-                ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `",2);
-                ft_putstr_fd(delimiter,2);
-                ft_putstr_fd("\')\n", 2);
-            }
+    while (1) {
+        char *line = readline("> ");
+        if (!line) {
+            // Ctrl-D pressed
+            ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `", STDERR_FILENO);
+            ft_putstr_fd(delimiter, STDERR_FILENO);
+            ft_putstr_fd("')\n", STDERR_FILENO);
+            break;
+        }
+        if (ft_strcmp(line, real_delim) == 0) {
             free(line);
             break;
         }
-
-        if (!quoted)
-        {
+        if (!quoted) {
             char *expanded = replace_vars(line, shell);
-            write(fd[1], expanded, ft_strlen(expanded));
-            write(fd[1], "\n", 1);
+            write(pipefd[1], expanded, ft_strlen(expanded));
+            write(pipefd[1], "\n", 1);
             free(expanded);
-        }
-        else
-        {
-            write(fd[1], line, ft_strlen(line));
-            write(fd[1], "\n", 1);
+        } else {
+            write(pipefd[1], line, ft_strlen(line));
+            write(pipefd[1], "\n", 1);
         }
         free(line);
     }
 
     free(real_delim);
-    close(fd[1]);
-    return (fd[0]);
+    close(pipefd[1]);
+    return pipefd[0];
 }
 
-
-static int process_in_heredoc(t_executor *current, t_lexer_list *lexer, t_shell *shell)
+/**
+ * Process input redirection: if HEREDOC, invoke create_heredoc with the
+ * raw token after HEREDOC; otherwise open filename. Returns OK or FAILED.
+ */
+static int process_in_heredoc(t_executor *cur, t_lexer_list *lex, t_shell *sh)
 {
-    // if (!lexer->next || lexer->next->type != EMPTY)
-    // {
-    //     ft_putstr_fd("minishell: syntax error near unexpected token\n", STDERR_FILENO);
-    //     return (FAILED);
-    // }
-    int heredoc_index = 0;
-    if (lexer->type == HEREDOC)
-    {
-        current->fd_in = create_heredoc(lexer->next->str, shell, heredoc_index++);
-        if (current->fd_in == -1)
-            return (FAILED);
-    }
-    else
-    {
-        current->fd_in = open(lexer->next->str, O_RDONLY);
-        if (current->fd_in == -1)
-        {
+    if (lex->type == HEREDOC) {
+        cur->fd_in = create_heredoc(lex->next->str, sh);
+        if (cur->fd_in == -1)
+            return FAILED;
+    } else {
+        cur->fd_in = open(lex->next->str, O_RDONLY);
+        if (cur->fd_in == -1) {
             ft_putstr_fd("minishell: ", STDERR_FILENO);
-            perror(lexer->next->str);
-            return (FAILED);
+            perror(lex->next->str);
+            return FAILED;
         }
     }
-    return (OK);
+    return OK;
 }
 
 static int process_command(t_executor *current, t_lexer_list *lexer)
